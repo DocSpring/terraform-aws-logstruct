6fd5835e7288063b5c7cba4d117d6a2dfcb1645a
Nathan Broadbent <git@ndbroadbent.com>
2025-09-06 22:46:18 +1200

    fix more ci issues

--- examples/metric_filter_basic/main.tf
+++ examples/metric_filter_basic/main.tf
@@ -1,14 +1,5 @@
 terraform {
-  required_providers {
-    logstruct = {
-      source  = "DocSpring/logstruct"
-      version = ">= 0.0.3"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
+  required_version = ">= 1.3.0"
 }
 
 module "email_delivered_metric" {
