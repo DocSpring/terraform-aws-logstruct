8d2727082942bc55d276705b6c107653013c32dd
Nathan Broadbent <git@ndbroadbent.com>
2025-09-06 15:15:42 +1200

    fix module

--- variables.tf
+++ variables.tf
@@ -9,17 +9,58 @@ variable "enable_subscription_filter" {
 }
 
 # Metric filter inputs
-variable "metric_filter_name" { type = string, default = null }
-variable "metric_log_group_name" { type = string, default = null }
-variable "metric_log_source" { type = string, default = null }
-variable "metric_log_event" { type = string, default = null }
-variable "metric_namespace" { type = string, default = null }
-variable "metric_metric_name" { type = string, default = null }
+variable "metric_filter_name" {
+  type    = string
+  default = null
+}
+
+variable "metric_log_group_name" {
+  type    = string
+  default = null
+}
+
+variable "metric_log_source" {
+  type    = string
+  default = null
+}
+
+variable "metric_log_event" {
+  type    = string
+  default = null
+}
+
+variable "metric_namespace" {
+  type    = string
+  default = null
+}
+
+variable "metric_metric_name" {
+  type    = string
+  default = null
+}
 
 # Subscription filter inputs
-variable "subscription_filter_name" { type = string, default = null }
-variable "subscription_log_group_name" { type = string, default = null }
-variable "subscription_log_source" { type = string, default = null }
-variable "subscription_log_event" { type = string, default = null }
-variable "subscription_destination_arn" { type = string, default = null }
+variable "subscription_filter_name" {
+  type    = string
+  default = null
+}
 
+variable "subscription_log_group_name" {
+  type    = string
+  default = null
+}
+
+variable "subscription_log_source" {
+  type    = string
+  default = null
+}
+
+variable "subscription_log_event" {
+  type    = string
+  default = null
+}
+
+variable "subscription_destination_arn" {
+  type    = string
+  default = null
+}
