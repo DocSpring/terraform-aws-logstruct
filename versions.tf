519f2bbab7b3d5512f31c09db8010c69ce936dd6
Nathan Broadbent <git@ndbroadbent.com>
2025-09-06 18:45:39 +1200

    fixed module

--- versions.tf
+++ versions.tf
@@ -1,14 +1,3 @@
 terraform {
   required_version = ">= 1.3.0"
-  required_providers {
-    logstruct = {
-      source  = "DocSpring/logstruct"
-      version = ">= 0.0.4"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
 }
-
